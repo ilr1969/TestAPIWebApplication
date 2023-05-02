52c4aeb56ce80906f0812b21d490e594ad357ae6
Дмитрий Савченко <ilr1969@yandex.ru>
2023-05-02 08:40:52 +0300

    Добавлена пара тестов.

--- TestAPIWebApplication/Program.cs
+++ TestAPIWebApplication/Program.cs
@@ -1,13 +1,10 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using OnlineShopWebApi;
 using TestAPIWebApplication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Начальное заполнение БД данными
-using (DatabaseContext databaseContext = new DatabaseContext())
+/*using (DatabaseContext databaseContext = new DatabaseContext())
 {
     databaseContext.Database.EnsureDeleted();
     databaseContext.Database.EnsureCreated();
@@ -15,7 +12,7 @@ using (DatabaseContext databaseContext = new DatabaseContext())
     var persons = GetSourceData.GetData();
     databaseContext.AddRange(persons);
     databaseContext.SaveChanges();
-}
+}*/
 
 // Add services to the container.
 
@@ -25,24 +22,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = AuthOptions.ISSUER,
-            ValidateAudience = true,
-            ValidAudience = AuthOptions.AUDIENCE,
-            ValidateLifetime = true,
-            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-            ValidateIssuerSigningKey = true,
-        };
-    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
@@ -53,31 +32,6 @@ builder.Services.AddSwaggerGen(option =>
         Description = "v1 API",
         Title = "TestApi",
 
-    });
-
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Bearer auth"
-    });
-
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Id = "Bearer",
-                    Type = ReferenceType.SecurityScheme
-                }
-            },
-            new string[] {}
-        }
     });
 });
 
---
+++ TestAPIWebApplicationTest/ApiControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using TestAPIWebApplication.Controllers;
+
+namespace TestAPIWebApplication.Test
+{
+    public class ApiControllerTest
+    {
+
+        private static readonly DatabaseContext databaseContext = new DatabaseContext();
+        private readonly ApiController ApiController = new ApiController(databaseContext);
+        private readonly ApiFilterArgs apiFilterArgs = new ApiFilterArgs();
+
+        [Fact]
+        private void GetPersons()
+        {
+            var result = ApiController.GetPersons(apiFilterArgs);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        private void GetPerson()
+        {
+            var person = ApiController.GetPerson("hqwuiehquikxhc5");
+            Assert.Equal("German Titov", person.Name);
+        }
+    }
+}
